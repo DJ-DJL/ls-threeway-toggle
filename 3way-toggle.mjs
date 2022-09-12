54246d15195baf83a97e0f240dd72a857b462f1d
Lee <lee.smith@vigeo-eiris.com>
2022-09-12 17:02:01 +0200

    Refactoring and adding mutation observer

--- 3way-toggle.mjs
+++ 3way-toggle.mjs
@@ -1,3 +1,31 @@
+class Option {
+  constructor(optionElement) {
+    this.optionElement = optionElement;
+    this.rdo = document.createElement('input');
+    this.lbl = document.createElement('label');
+    this.updateFromOption();
+  }
+  set value(value) {
+    this.rdo.setAttribute('value', value);
+    this.rdo.value = value;
+  }
+  set text(text) {
+    this.lbl.innerText = text;
+    this.lbl.setAttribute('data-boldSize', text);
+  }
+  get value() { return this.rdo.value; }
+  get text() { return this.lbl.innerText; }
+  updateFromOption() {
+    if (this.optionElement) {
+      this.text = this.optionElement.innerText;
+      this.value = this.optionElement.getAttribute('value') || this.optionElement.innerText;
+    } else {
+      this.text = 'Missing Option child element';
+      this.value = null;
+    }
+  }
+}
+
 const svgUp = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="-33 -5 116 109" width="32" version="2">
   <!-- 0,30  50,30  25,73.3 -->
@@ -30,91 +58,92 @@ customElements.define('three-way-toggle',
     #value;
     #sessionStorageID = `three-way-${this.id}`;
     #setValue(value) {
+      const prevValue = this.#value;
       this.#value = value;
-      this.setAttribute('value', value);
-      for (const option of this.#options) {
-        option.option.checked = (option.value === value);
+      this.#pauseMutationObserver();
+      try {
+        this.setAttribute('value', value);
+        for (const option of this.#options) {
+          if (option.option) {
+            option.option.checked = (option.value === value);
+          }
+        }
+      } finally {
+        this.#resumeMutationObserver();
       }
       sessionStorage.setItem(this.#sessionStorageID, value);
       // add event stuff here
-      this.dispatchEvent(new Event('change'));
+      if (prevValue != this.#value) {
+        this.dispatchEvent(new Event('change'));
+      }
     }
     get value() {
       return this.#value;
     }
     set value(value) {
-      console.log('set value', value, [this.#rdo1, this.#rdo2, this.#rdo3].find(rdo => rdo.value === value))
-      const rdo = [this.#rdo1, this.#rdo2, this.#rdo3].find(rdo => rdo.value === value);
+      const rdos = this.#options.map(o => o.rdo);
+      const rdo = rdos.find(rdo => rdo.value === value);
       if (rdo) {
         rdo.checked = true;
       }
       this.#setValue(value);
     }
-    #rdo1
-    #rdo2
-    #rdo3
-    #lbl1
-    #lbl2
-    #lbl3
     #lbl3b
     #options
-
-    #setOption(rdo, lbl, option) {
-      if (rdo) {
-        rdo.setAttribute('value', option.value);
+    #mutationObserver
+    #resumeMutationObserver() {
+      if (this.#mutationObserver) {
+        this.#mutationObserver.observe(this, {
+          attributes: true,
+          childList: true,
+          subtree: true,
+        });
       }
-      if (lbl) {
-        lbl.setAttribute(`data-boldsize`, option.text)
-        lbl.innerText = option.text;
+    }
+    #pauseMutationObserver() {
+      if (this.#mutationObserver) {
+        this.#mutationObserver.disconnect();
       }
     }
     constructor() {
       super();
-      this.#options = [...this.querySelectorAll('option')].map(option => ({
-        option,
-        text: option.innerText,
-        value: option.getAttribute('value') || option.innerText,
-      }));
+      this.#options = [...this.querySelectorAll('option')].map(option => new Option(option));
       if (this.#options.length !== 3) {
-        throw new Error('three-way-toggle elements require exactly 3 option children');
+        console.warn('Insufficient option children to fully construct now. Doing the best I can')
+        // throw new Error('three-way-toggle elements require exactly 3 option children');
+        while (this.#options.length !== 3) {
+          this.#options.push(new Option());
+        }
       }
       const shadowRoot = this.attachShadow({ mode: 'open' });
-      this.#rdo1 = document.createElement('input');
-      this.#rdo2 = document.createElement('input');
-      this.#rdo3 = document.createElement('input');
-      this.#lbl1 = document.createElement('label');
-      this.#lbl2 = document.createElement('label');
-      this.#lbl3 = document.createElement('label');
+
       this.#lbl3b = document.createElement('label');
-      this.#setOption(this.#rdo1, this.#lbl1, this.#options[0]);
-      this.#setOption(this.#rdo2, this.#lbl2, this.#options[1]);
-      this.#setOption(this.#rdo3, this.#lbl3, this.#options[2]);
-      this.#setOption(null, this.#lbl3b, this.#options[2]);
+      this.#lbl3b.innerText = this.#options[2].text;
 
-      setAttributes(this.#rdo1, { id: 'A', name: 'choice', 'type': 'radio' });
-      setAttributes(this.#rdo2, { id: 'B', name: 'choice', 'type': 'radio' });
-      setAttributes(this.#rdo3, { id: 'C', name: 'choice', 'type': 'radio' });
-      this.#lbl1.classList.add('A');
-      this.#lbl2.classList.add('B');
-      this.#lbl3.classList.add('C');
+      setAttributes(this.#options[0].rdo, { id: 'A', name: 'choice', 'type': 'radio' });
+      setAttributes(this.#options[1].rdo, { id: 'B', name: 'choice', 'type': 'radio' });
+      setAttributes(this.#options[2].rdo, { id: 'C', name: 'choice', 'type': 'radio' });
+      this.#options[0].lbl.classList.add('A');
+      this.#options[1].lbl.classList.add('B');
+      this.#options[2].lbl.classList.add('C');
       this.#lbl3b.classList.add('min-size');
-      setAttributes(this.#lbl1, { for: 'A' });
-      setAttributes(this.#lbl2, { for: 'B' });
-      setAttributes(this.#lbl3, { for: 'C' });
+      setAttributes(this.#options[0].lbl, { for: 'A' });
+      setAttributes(this.#options[1].lbl, { for: 'B' });
+      setAttributes(this.#options[2].lbl, { for: 'C' });
 
-      this.#rdo1.addEventListener('change', () => this.#setValue(this.#rdo1.value));
-      this.#rdo2.addEventListener('change', () => this.#setValue(this.#rdo2.value));
-      this.#rdo3.addEventListener('change', () => this.#setValue(this.#rdo3.value));
+      this.#options[0].rdo.addEventListener('change', () => this.#setValue(this.#options[0].rdo.value));
+      this.#options[1].rdo.addEventListener('change', () => this.#setValue(this.#options[1].rdo.value));
+      this.#options[2].rdo.addEventListener('change', () => this.#setValue(this.#options[2].rdo.value));
 
       const svg = getSVG(this.getAttribute('direction') || 'down');
       svg.querySelector('circle').style.transitionDuration = '0s';
       const styles = getStyles(this.getAttribute('direction') || 'down');
-      this.value = sessionStorage.getItem(this.#sessionStorageID) || this.#rdo1.value;
+      this.value = sessionStorage.getItem(this.#sessionStorageID) || this.#options[0].rdo.value;
 
       const divArrangement = document.createElement('div');
-      divArrangement.append(svg, this.#lbl3);
+      divArrangement.append(svg, this.#options[2].lbl);
       this.setAttribute('data-boldsize', this.#options[2].text);
-      this.shadowRoot.append(styles, this.#rdo1, this.#rdo2, this.#rdo3, this.#lbl1, divArrangement, this.#lbl2, this.#lbl3b);
+      this.shadowRoot.append(styles, ...this.#options.map(o => o.rdo), this.#options[0].lbl, divArrangement, this.#options[1].lbl, this.#lbl3b);
       if (Object.prototype.hasOwnProperty.call(this, 'value')) {
         const value = this.value;
         delete this.value;
@@ -123,14 +152,23 @@ customElements.define('three-way-toggle',
       setTimeout(() => {
         svg.querySelector('circle').style.transitionDuration = null;
         const minTotalWidth = this.#lbl3b.clientWidth;
-        console.log(minTotalWidth);
         const divWidthOn2 = divArrangement.clientWidth / 2;
-        this.#lbl1.style.marginLeft = ` ${Math.max(0, (minTotalWidth / 2) - (this.#lbl1.clientWidth + divWidthOn2))}px`;
-        this.#lbl2.style.marginRight = `${Math.max(0, (minTotalWidth / 2) - (this.#lbl2.clientWidth + divWidthOn2))}px`;
+        this.#options[0].lbl.style.marginLeft = ` ${Math.max(0, (minTotalWidth / 2) - (this.#options[0].lbl.clientWidth + divWidthOn2))}px`;
+        this.#options[1].lbl.style.marginRight = `${Math.max(0, (minTotalWidth / 2) - (this.#options[1].lbl.clientWidth + divWidthOn2))}px`;
         this.#lbl3b.style.display = 'none';
       }, 1);
 
-
+      this.#mutationObserver = new MutationObserver((...args) => this.#mutation(...args));
+      this.#resumeMutationObserver();
+    }
+    #mutation(mutationList, observer) {
+      const currentValue = this.value;
+      for (const [idx, optionElement] of Object.entries(this.querySelectorAll('option'))) {
+        this.#options[idx].optionElement = optionElement;
+        this.#options[idx].updateFromOption();
+        // this.#setOption(this.#options[idx]);
+      }
+      this.value = currentValue;
     }
   }
 );
